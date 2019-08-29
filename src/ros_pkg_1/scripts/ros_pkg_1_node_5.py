32a33fcf1f8e3f8e1637cde4eaca03212b4add15
you <you@example.com>
2019-08-29 10:55:36 +0800

    文件编码，变量全局改为实例

--- src/ros_pkg_1/scripts/ros_pkg_1_node_5.py
+++ src/ros_pkg_1/scripts/ros_pkg_1_node_5.py
@@ -1,37 +1,38 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import rospy
 import actionlib
 import time
+import traceback
 
 from ros_pkg_1.msg import *
 from std_msgs.msg import *
 
 ros_pkg_1_node_5_log_prefix = 'ros_pkg_1_node_5'
 
-wait_seconds = 0.0
-
-all_wait_seconds = 10
-
 
 class DoDishesServer:
     def __init__(self):
+        self.wait_seconds = 0.0
+        self.all_wait_seconds = 10
+
         self.server = actionlib.SimpleActionServer('do_dishes', DoDishesAction, self.execute, False)
         self.server.start()
 
     def execute(self, goal):
-        # Do lots of awesome groundbreaking robot stuff here
-        rospy.loginfo('%s receive dishwasher_id=%s', ros_pkg_1_node_5_log_prefix,
-                      self.server.current_goal.get_goal().dishwasher_id)
-        global wait_seconds
-        while wait_seconds != all_wait_seconds:
-            wait_seconds = wait_seconds + 1
-            time.sleep(0.5)
-            self.server.publish_feedback(DoDishesFeedback(percent_complete=wait_seconds / all_wait_seconds * 100))
-        rospy.loginfo('%s actionlib execute completed!', ros_pkg_1_node_5_log_prefix)
-        self.server.set_succeeded(result=DoDishesResult(total_dishes_cleaned=wait_seconds), text='all dishes done!')
-        wait_seconds = 0.0
-
+        try:
+            # Do lots of awesome groundbreaking robot stuff here
+            rospy.loginfo('%s receive dishwasher_id=%s', ros_pkg_1_node_5_log_prefix,
+                          self.server.current_goal.get_goal().dishwasher_id)
+            while self.wait_seconds != self.all_wait_seconds:
+                self.wait_seconds = self.wait_seconds + 1
+                time.sleep(0.5)
+                self.server.publish_feedback(DoDishesFeedback(percent_complete=self.wait_seconds / self.all_wait_seconds * 100))
+            rospy.loginfo('%s actionlib execute completed!', ros_pkg_1_node_5_log_prefix)
+            self.server.set_succeeded(result=DoDishesResult(total_dishes_cleaned=self.wait_seconds), text='all dishes done!')
+        except Exception as e:
+            rospy.logerr('DoDishesServer.execute Exception, e=%s', traceback.format_exc(e))
 
 
 if __name__ == '__main__':
